e1bacf0f750867165b39830b791482285e796cc8
Alex Hidalgo <aj1hidalgo@gmail.com>
2015-01-14 15:43:40 -0600

    Planned out casino game.

---
+++ Day-7/homework/main.js
@@ -0,0 +1,12 @@
+//Slot Machine Game
+
+//Directions for making game
+
+//random number must match for user to be awarded.
+//User will be initialized with a prompt asking them to play the game upon document.ready
+//Prompt: Yes or no?
+// yes = 3 math.randoms and if they match then you get a prompt saying "you won!"
+// no = cancel out of game with notification of cancellation
+
+//Start with creating the matching game
+//Then use switch statements for yes and no and maybe
