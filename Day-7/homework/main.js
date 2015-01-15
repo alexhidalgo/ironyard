45be67967032fa0c915fd85dc43cbc2042d62336
Alex Hidalgo <aj1hidalgo@gmail.com>
2015-01-14 21:29:15 -0600

    completed slot machine game. Needs polish and better UX for example using window.confirm instead of window.alert to ask users if they would like to continue playing.

--- Day-7/homework/main.js
+++ Day-7/homework/main.js
@@ -10,3 +10,57 @@
 
 //Start with creating the matching game
 //Then use switch statements for yes and no and maybe
+
+//Casino Game Function
+
+// function firstNum(min, max) {
+//   return Math.floor(Math.random() * (3 - 1)) + 1;
+// }
+
+// function secondNum(min, max) {
+//   return Math.floor(Math.random() * (3 - 1)) + 1;
+// }
+
+// function thirdNum(min, max) {
+//   return Math.floor(Math.random() * (3 - 1)) + 1;
+// }
+
+// Casino Game upon doc.ready
+
+$( document ). ready(function() {
+
+	var firstNum = Math.floor(Math.random() * (4-1) + 1);
+	var secondNum = Math.floor(Math.random() * (4-1) + 1);
+	var thirdNum = Math.floor(Math.random() * (4-1) + 1);
+	console.log(firstNum, secondNum, thirdNum);
+
+
+	if(firstNum === secondNum && secondNum === thirdNum) {
+		console.log("You win!");
+		window.alert(firstNum + secondNum + thirdNum + "You win!");
+	}	else {
+		console.log("You lose!");
+		window.alert(firstNum + secondNum + thirdNum + "You lose!");
+	}
+
+});
+
+
+
+
+// for (i=0; i<5; i++) {
+
+
+// }
+
+// //Prompt Casino Game upon Doc.ready
+
+// $( document ).ready(function() {
+// 	var firstPrompt = window.prompt('Would you like to play this slot machine?');
+// 	if (firstPrompt == "yes") {
+
+// 	}	else {
+// 		console.log("You keep your money!");
+// 	}
+// });
+
